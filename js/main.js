3d6a11c7e72a5d7e974ddeab4d973a3790b65465
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2017-07-18 20:56:21 +0530

    Initial Commit
    Made the HTML/CSS of Landing Page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // ENABLE SCROLLING ON REDIRECTION TO VARIOUS DIVs
+    $('.scrollspy').scrollSpy({
+        scrollOffset: $('header').height()  // Remove the scroll offset defaulted to 200px
+    });
+
+    // Enable Collapsible Button
+    $(".button-collapse").sideNav();
+});
