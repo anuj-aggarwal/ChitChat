33eb5181559c3e341be0d0496dd201a2a258b02f
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2017-08-13 12:04:52 +0530

    Making Profile Sidebar: Fixed HTML SideNav
    - On large Screens, fixed SideBar
    - On smaller screens, Slideout menu

---
+++ public_static/js/common.js
@@ -0,0 +1,8 @@
+$(function(){
+    // Initialize the Slideout Side-Nav
+    $('.button-collapse').sideNav({
+        edge: 'left', // Horizontal Origin: Left Edge
+        closeOnClick: true, // Closes on <a> clicks
+        draggable: true // Drag to open on touch screens
+    });
+});
