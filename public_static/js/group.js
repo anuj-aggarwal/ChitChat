1887959cd9a196787bb1cd738cfee4b8646c1f4c
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-06-13 18:06:20 +0530

    Refactor Frontend Scripts

--- public_static/js/group.js
+++ public_static/js/group.js
@@ -1,90 +1,84 @@
-let list;   // ul containing chats
-let sendButton;
-let input;
-let messagesContainer;
-let timeoutId = null;
+$(() => {
+    const $list = $("#messages-list"); // ul containing chats
+    const $sendButton = $("#send-button");
+    const $input = $("#input");
+    const $messagesContainer = $("#messages-container");
 
-let username;   // Username of current User
+    let timeoutId = null;   // Id of timeout to clear typing message
+    let username;   // Username of current User
 
-$(function(){
-    list = $("#messages-list");
-    sendButton = $("#send-button");
-    input = $("#input");
-    messagesContainer = $("#messages-container");
 
     // Connect with the Server via Socket
     const socket = io("/groups");
 
     // Get the Username of current user by making an AJAX request
-    $.get("/details", function(data){
+    $.get("/details", (data) => {
         username = data.username;
 
         // Emit the Current URL and isChannel for server to get the chatID
         socket.emit("data", {
             url: window.location.pathname,
-            username: username
+            username
         });
     });
 
     // Get old messages from the server
-    socket.on("Messages", function(chats){
+    socket.on("Messages", (chats) => {
         // Clear the list
-        list.html("");
+        $list.html("");
         // For each message, append to the list
-        for (const chat of chats) {
+        chats.forEach(chat => {
             // If the message is a normal message
-            if(chat.for.length===0) {
-                appendMessage(chat);
+            if (chat.for.length === 0) {
+                appendMessage($list, chat);
             }
             // If the message is a whisper meant for current User
-            else if(chat.for.indexOf(username)!=-1) {
-                appendWhisper(chat);
+            else if (chat.for.indexOf(username) !== -1) {
+                appendWhisper($list, chat);
             }
-        }
+        });
         // Scroll to bottom of container
-        updateScroll();
+        updateScroll($messagesContainer);
     });
 
     // Append new messages when received
-    socket.on("message", function(chat){
+    socket.on("message", (chat) => {
         // Remove any typing messages if present
-        $("#typing").remove();
-        clearTimeout(timeoutId);
-        timeoutId = null;
+        clearTypingMessage(timeoutId);
 
         // Append the new message
         // If the message is a normal message
-        if(chat.for.length===0){
-            appendMessage(chat);
+        if (chat.for.length === 0) {
+            appendMessage($list, chat);
         }
         // If the message is a whisper meant for current User
-        else if(chat.for.indexOf(username)!==-1){
-            appendWhisper(chat);
+        else if (chat.for.indexOf(username) !== -1) {
+            appendWhisper($list, chat);
         }
-        
+
         // Scroll to bottom of container
-        updateScroll();
+        updateScroll($messagesContainer);
     });
 
 
     // Append typing message when received after removing previous typing messages
     // And remove current typing message after 1 second
-    socket.on("typing", function(username){
+    socket.on("typing", (username) => {
         // Remove previous typing messages if present
-        $("#typing").remove();
-        clearTimeout(timeoutId);
-        timeoutId = null;
+        clearTypingMessage(timeoutId);
+
         // Display typing message
-        list.append(`
+        $list.append(`
             <li id="typing">
                 <b>${username} is typing.....</b>
             </li>
         `);
+
         // Scroll to bottom of container
-        updateScroll();
+        updateScroll($messagesContainer);
 
         // Remove the message after 1 second
-        timeoutId = setTimeout(function(){
+        timeoutId = setTimeout(() => {
             $("#typing").remove();
         }, 500);
     });
@@ -94,22 +88,22 @@ $(function(){
     //   EVENT LISTENERS
     // --------------------
 
-    sendButton.click(function(){
+    $sendButton.click(() => {
         // Emit the message along with Sender
         socket.emit("new message", {
             sender: username,
-            body: input.val()
+            body: $input.val()
         });
         // Clear the input
-        input.val("");
+        $input.val("");
     });
 
     // Send the message on pressing ENTER in input box
     // emit typed event if any other key
-    input.on("keypress", function(event){
-        if(event.keyCode === 13)
-            sendButton.click();
-        else{
+    $input.on("keypress", (event) => {
+        if (event.keyCode === 13)
+            $sendButton.click();
+        else {
             // Send typed event to Server with username
             socket.emit("typed", username);
         }
@@ -118,24 +112,31 @@ $(function(){
 });
 
 // Appends a normal message to list
-function appendMessage(message) {
-    list.append(`
+const appendMessage = ($list, message) => {
+    $list.append(`
         <li>
             <b>${message.sender}:</b> ${message.body}                
         </li>
     `);
-}
+};
 
 // Appends a whisper to list
-function appendWhisper(message) {
-    list.append(`
+const appendWhisper = ($list, message) => {
+    $list.append(`
         <li>
             <b>${message.sender}:</b> <span class="grey-text">${message.body}</span>                
         </li>
     `);
-}
+};
 
 // Scrolls the container to the bottom
-function updateScroll(){
-    messagesContainer.scrollTop(messagesContainer.prop("scrollHeight"));
-}
+const updateScroll = ($messagesContainer) => {
+    $messagesContainer.scrollTop($messagesContainer.prop("scrollHeight"));
+};
+
+// Clears any typing message
+const clearTypingMessage = (timeoutId) => {
+    $("#typing").remove();
+    clearTimeout(timeoutId);
+    timeoutId = null;
+};
