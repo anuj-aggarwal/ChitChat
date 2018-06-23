4a0f81605fd91f12eb6f49c5a96000fb9dd458f4
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-06-23 15:50:07 +0530

    Fix CONFIG Variables for Heroku

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ switch (process.env.NODE_ENV) {
             SESSION_SECRET: process.env.SESSION_SECRET,
             COOKIE_SECRET: process.env.COOKIE_SECRET,
             CLOUDINARY: {
-                CLOUD_NAME: process.env.CLOUD_NAME,
-                API_KEY: process.env.API_KEY,
-                API_SECRET: process.env.API_SECRET
+                CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+                API_KEY: process.env.CLOUDINARY_API_KEY,
+                API_SECRET: process.env.CLOUDINARY_API_SECRET
             }
         };
         break;
